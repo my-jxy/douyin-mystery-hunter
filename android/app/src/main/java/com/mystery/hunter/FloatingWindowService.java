c146f8a38957c3749b6bb85cf56c36d15330b168
my-jxy <252670567+my-jxy@users.noreply.github.com>
2026-06-19 12:13:00 +0800

    fix: add missing @Nullable import in FloatingWindowService

--- android/app/src/main/java/com/mystery/hunter/FloatingWindowService.java
+++ android/app/src/main/java/com/mystery/hunter/FloatingWindowService.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.ImageView;
 import androidx.core.app.NotificationCompat;
+import androidx.annotation.Nullable;
 
 public class FloatingWindowService extends Service {
 
